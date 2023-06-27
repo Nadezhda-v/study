62b8fca2fe10a87ee1063728ad3b52c6c8533569
Nadezhda Vorotova <work-nadezhda-v@yandex.ru>
2023-06-27 21:11:58 +0400

    add a second block

--- Module_1/lesson_01-1/script.js
+++ Module_1/lesson_01-1/script.js
@@ -1,10 +1,26 @@
 'use strict';
 
-const nameItem = 'book';
-const quantityItems = 2;
-const categoryItem = 'literature';
-const priceItem = 5.6;
+{
+    const nameItem = 'book';
+    const quantityItems = 2;
+    const categoryItem = 'literature';
+    const priceItem = 5.6;
 
-let sum = quantityItems * priceItem + ' $';
+    let sum = quantityItems * priceItem + ' $';
 
-console.log('Общая сумма товара: ', sum);
+    console.log('Наименование товара: ', nameItem);
+    console.log('Общая сумма товара: ', sum);
+}
+
+{
+    const nameItem = 'magazine';
+    const quantityItems = 1;
+    const categoryItem = 'literature';
+    const priceItem = 2.1;
+
+    let sum = quantityItems * priceItem + ' $';
+
+    console.log('Наименование товара: ', nameItem);
+    console.log('Общая сумма товара: ', sum);
+}
+

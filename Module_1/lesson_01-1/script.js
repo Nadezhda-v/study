2d7b1212ee6a5b11b575e7e7e4d10c558c3ba240
Nadezhda Vorotova <work-nadezhda-v@yandex.ru>
2023-06-27 21:02:03 +0400

    add lesson_01

---
+++ Module_1/lesson_01-1/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const nameItem = 'book';
+const quantityItems = 2;
+const categoryItem = 'literature';
+const priceItem = 5.6;
+
+let sum = quantityItems * priceItem + ' $';
+
+console.log('Общая сумма товара: ', sum);
---
+++ Module_1/lesson_01/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const nameItem = 'book';
+const quantityItems = 2;
+const categoryItem = 'literature';
+const priceItem = 5.6 + ' $';
+
+console.log('Наименование товара: ', nameItem);

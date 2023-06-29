4ca33f21851e06cf3185ff950958aa79d4526896
Nadezhda Vorotova <work-nadezhda-v@yandex.ru>
2023-06-29 15:38:04 +0400

    Испралены условия вывода countTax

--- Module_1/lesson_04 -1/tasksHard_2.js
+++ Module_1/lesson_04 -1/tasksHard_2.js
@@ -2,24 +2,17 @@ const profit = +prompt('Введите сумму вашего дохода за
 
 let countTax;
 let addTax;
-let rest;
 let min = 15000;
 let max = 50000;
 
-if(profit > max) {
-    rest = profit - max;
-    addTax = rest * 0.3;
-    countTax = (max - min) * 0.2 + min * 0.13 + addTax;
-}
-
-if(profit > min) {
-    rest = profit - min;
-    addTax = rest * 0.2;
-    countTax = min * 0.13 + addTax;
-}
-
-if(profit > 0) {
-    countTax = profit * 0.13;
+if (profit <= min) {
+    countTax = profit * 0.13; 
+} else if (profit <= max) {
+    addTax = profit - min;
+    countTax = min * 0.13 + addTax * 0.2; 
+} else {
+    addTax = profit - max;
+    countTax = min * 0.13 + (max - min) * 0.2 + addTax * 0.3; 
 }
 
 console.log('Сумма вашего налога составит:', `${countTax}`);

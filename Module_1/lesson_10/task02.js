b9651bafb9cd98ab931e054ae30689b0823e7a45
Nadezhda Vorotova <work-nadezhda-v@yandex.ru>
2023-07-04 18:04:53 +0400

    Посчитать количество уникальных IP-адресов

--- Module_1/lesson_10/task02.js
+++ Module_1/lesson_10/task02.js
@@ -1,6 +1,11 @@
 'use strict'
 
+import {listIPv4} from './ipv4.js';
+
 const getUniqueIP = (arr) => {
-    const newArr = [...new Set(arr)].length;
-    return newArr;
+  const newArr = [...new Set(arr)].length;
+  return newArr;
 }
+
+const uniqueIP = getUniqueIP(listIPv4);
+console.log('uniqueIP: ', uniqueIP);
